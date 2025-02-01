088f5bb42980651fc8aa30080a80c8add7e9d193
MrLucasapl <lucas.apereira20@gmail.com>
2025-02-01 11:12:25 -0300

    add metas and bug migrations api

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,36 +3,45 @@ import { join } from 'node:path';
 import database from 'infra/database';
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-  const defaultMigrationOptios = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join('infra', 'migrations'),
-    direction: 'up',
-    verbose: true,
-    migrationsTable: 'pgmigrations',
-  };
-
-  if (request.method === 'GET') {
-    const pendingMigations = await migrationRunner(defaultMigrationOptios);
-    await dbClient.end();
-    return response.status(200).json(pendingMigations);
+  const allowedMethods = ['GET', 'POST'];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).send(`Método ${request.method} não permitido`);
   }
 
-  if (request.method === 'POST') {
-    const migratedMigations = await migrationRunner({
-      ...defaultMigrationOptios,
-      dryRun: false,
-    });
+  let dbClient;
 
-    await dbClient.end();
-    
-    if (migratedMigations.length > 0) {
-      return response.status(201).json(migratedMigations);
+  try {
+    dbClient = await database.getNewClient();
+    const defaultMigrationOptios = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join('infra', 'migrations'),
+      direction: 'up',
+      verbose: true,
+      migrationsTable: 'pgmigrations',
+    };
+
+    if (request.method === 'GET') {
+      const pendingMigations = await migrationRunner(defaultMigrationOptios);
+      return response.status(200).json(pendingMigations);
     }
 
-    return response.status(200).json(migratedMigations);
-  }
+    if (request.method === 'POST') {
+      const migratedMigations = await migrationRunner({
+        ...defaultMigrationOptios,
+        dryRun: false,
+      });
 
-  return response.status(405).end();
+      if (migratedMigations.length > 0) {
+        return response.status(201).json(migratedMigations);
+      }
+
+      return response.status(200).json(migratedMigations);
+    }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  } finally {
+    await dbClient.end();
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+import Head from 'next/head';
+
 function Home() {
-  return <h1>Home - seja bem vindo!</h1>;
+  return (
+    <div>
+      <Head>
+        <title>Worldbit</title>
+        <meta charset="UTF-8" />
+        <meta
+          name="facebook-domain-verification"
+          content="jkza6sydnb4434g4ep08qpd44mhvmi"
+        />
+      </Head>
+      <h1>Home - seja bem vindo!</h1>
+    </div>
+  );
 }
 
 export default Home;
